a0c729f877b1a7215bed917899591ae8d5a874a6
Egle Laboureur <egle.laboureur@gmail.com>
2023-12-14 15:00:35 +0100

    added map method to component NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,24 +3,31 @@ function NavBar(props) {
 let setPokemonIndex=props.setPokemonIndex;
 let pokemonList=props.pokemonList;
 
-  const handleNextClick = () => {
-    setPokemonIndex((prevIndex) => (prevIndex < pokemonList.length - 1 ? prevIndex + 1 : prevIndex));
+  // const handleNextClick = () => {
+  //   setPokemonIndex((prevIndex) => (prevIndex < pokemonList.length - 1 ? prevIndex + 1 : prevIndex));
     
-  };
+  // };
   
-  const handlePrevClick = () => {
-    setPokemonIndex((prevIndex) => (prevIndex >0 ? prevIndex - 1 : prevIndex));
+  // const handlePrevClick = () => {
+  //   setPokemonIndex((prevIndex) => (prevIndex >0 ? prevIndex - 1 : prevIndex));
     
-  };
-    
+      
     return (
    
         <div>
-          <button onClick={handlePrevClick}>Précédent</button>
-          <button onClick={handleNextClick}>Suivant</button>
+          {/* <button onClick={handlePrevClick}>Précédent</button>
+          <button onClick={handleNextClick}>Suivant</button> */}
+      
+            {pokemonList.map((pokemon, index) => (
+                <button key={index} onClick={()=>setPokemonIndex (index)}>{pokemon.name}</button> 
+              
+            ))
+           }
+       
         </div>
            
     );
-  }
+  
+};
   
   export default NavBar;
